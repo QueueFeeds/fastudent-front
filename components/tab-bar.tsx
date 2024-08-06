69d99c732f7f2ad791e9b5c6ec6d32d4b8ab6ca4
Dong-tak <lonu68325@gmail.com>
2024-08-06 16:16:36 +0900

    ☔️ Add: commit message test
    ## ✅ What I did
    - - commit message test
    
    ## #️⃣ Linked issues

--- components/tab-bar.tsx
+++ components/tab-bar.tsx
@@ -16,7 +16,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-// 테스트
+// akgnl awoihgoasgho;
 export default function TabBar() {
   const pathname = usePathname();
   return (
